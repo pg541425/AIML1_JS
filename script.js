5c8a8fd6ab1da50bfab1ae83d387ac6e8b4c6b14
pg541425 <pg541425@gmail.com>
2026-02-23 14:58:21 +0530

    Completed

--- script.js
+++ script.js
@@ -29,8 +29,8 @@
 // }
 
 
-// var a=100; var b=13; var c=10; var linebreak = "<br>";
-// document.write("a + b + c =");
-// result =a+b+c;
-// document.write(result);
-// document.write(linebreak);
+var a=100; var b=13; var c=10; var linebreak = "<br>";
+document.write("a + b + c =");
+result =a+b+c;
+document.write(result);
+document.write(linebreak);
