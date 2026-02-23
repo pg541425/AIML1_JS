f6302a7766c015d00a0aaf091babe9a30c47b5ae
pg541425 <pg541425@gmail.com>
2026-02-23 15:42:07 +0530

    Finished day 2

--- script_day2.js
+++ script_day2.js
@@ -1,7 +1,27 @@
-var s=10;
-var b=20;
-var linebreak='<br>';
-document.write("(s==b) :");
-result=(s==b);
-document.write(linebreak);
-document.write(result);
+// var s=10;
+// var b=20;
+// var linebreak='<br>';
+// document.write("(s==b) :");
+// result=(s==b);
+// document.write(linebreak);
+// document.write(result);
+
+//write an assignment to check max b/w 3 variables .... (HW)
+
+//----- LOOPS -------
+
+// var count;
+// for(count =0;count<10;count++){
+//     document.write(count);
+//     document.write("<br>");
+// }
+
+//for in loop
+
+// const person={
+//     fname:"Siya",
+//     fage:21
+// };
+// for(let x in person ){
+//     console.log("person details:" + x + ":"+person[x])
+// }
